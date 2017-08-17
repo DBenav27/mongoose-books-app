dae3e221283036356f56f566b91d7c5ade0883c5
Daniel Benavidez <danielbenavidez27@gmail.com>
2017-08-16 20:51:11 -0500

    added books.js

---
+++ models/books.js
@@ -0,0 +1,14 @@
+//  book.js
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var BookSchema = new Schema({
+  title: String,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'Author'
+  },
+  image: String,
+  releaseDate: String
+});
